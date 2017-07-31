625de9674d8c164c1fe9e0568a5d47bd26b1ee98
prernaanchan <prerna.anchan@gmail.com>
2017-07-30 20:30:21 -0700

    second Commit

---
+++ table.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    var table = $('#data-table').DataTable({
+        "ajax" : "records.json",
+        "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
+        "sort" : true,
+        "searching" : true,
+        "scrollY"  : "400px",
+        "columns" : [
+            {  "data" : "name" },
+            {  "data" : "designation" },
+            {  "data" : "nationality" }
+        ]
+    });
+    //Individual Column search
+    $('#data-table tfoot th').each(function(){
+        var title = $('#data-table thead th').eq($(this).index()).text();
+        $(this).html( '<input type="text" placeholder="Search ' + title +'" />' );
+    });
+        table.columns().every( function () {
+        var that = this;
+
+        $(this.footer() ).on( 'keyup change', function () {
+        that.search(this.value).draw();
+        } );
+    } );
+
+    // Show/hide Columns
+    $('.showHideColumn').on( 'click', function () { 
+        // Get the column API object
+        var column = table.column( $(this).attr('data-columnindex') ); 
+        // Toggle the visibility
+        column.visible( ! column.visible() );
+    } );   
+});
